c63660fb6e8743c7f5c135ea0cb08bae3b89c773
andyfu <815965325@qq.com>
2019-09-17 20:21:10 +0800

    增加module_2

---
+++ module_2/src/main/java/com/fxj/module_2/Module2MainActivity.java
@@ -0,0 +1,14 @@
+package com.fxj.module_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Module2MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module_2_main);
+    }
+}
