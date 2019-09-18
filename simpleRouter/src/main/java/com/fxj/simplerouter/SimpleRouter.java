571c739019c2e6d530252f3e440acbe16bd5aaa4
andyfu <815965325@qq.com>
2019-09-18 23:04:23 +0800

    修改com.fxj.simplerouter.SimpleRouter#init方法。

--- simpleRouter/src/main/java/com/fxj/simplerouter/SimpleRouter.java
+++ simpleRouter/src/main/java/com/fxj/simplerouter/SimpleRouter.java
@@ -4,13 +4,18 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 
+import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-
+import java.util.List;
+import java.util.ArrayList;
+import dalvik.system.DexFile;
 public class SimpleRouter {
 
     private static final String TAG=SimpleRouter.class.getSimpleName();
@@ -36,6 +41,49 @@ public class SimpleRouter {
 
     public void init(Application application){
         mContext = application;
+        List<String> classNames= getClassName("com.fxj.irouterimpl");
+        Log.d(TAG,"**init**classNames.size()="+classNames.size());
+        if(classNames!=null&&classNames.size()>0){
+            for(String className:classNames){
+                try {
+                   Class<?> aClass =Class.forName(className);
+                   if(IRouter.class.isAssignableFrom(aClass)){
+                       IRouter iRouter= (IRouter) aClass.newInstance();
+                       iRouter.loadInto();
+                   }
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private List<String> getClassName(String packageName){
+        List<String> classNameList=new ArrayList<String>();
+        String path=null;
+        try {
+            /*通过PackageManager获取到ApplicationInfo,然后获取到apk的完整路径*/
+            path= mContext.getPackageManager().getApplicationInfo(mContext.getPackageName(), 0).sourceDir;
+            DexFile df = new DexFile(path);//根据apk完整路径获取编译后的dex文件
+            Enumeration<String> enumeration = df.entries();//获取df中的元素  这里包含了所有可执行的类名 该类名包含了包名+类名的方式
+            while (enumeration.hasMoreElements()) {//遍历
+
+                String className = (String) enumeration.nextElement();/*获取类名*/
+
+                if (className.contains(packageName)) {//在当前所有可执行的类里面查找包含有该包名的所有类
+                    classNameList.add(className);
+                }
+            }
+        }  catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return classNameList;
     }
 
     public void putActivity(String path,Class<? extends Activity> clazz){
